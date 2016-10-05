d00b40d28fe3fcb71c95e1a7a42bfb47bdd658b2
Michael Harrison <michael.harrison2010@gmail.com>
2016-10-04 18:56:37 -0700

    First commit: exploring range() funciton

---
+++ Python Range Function Exercise.py
@@ -0,0 +1,21 @@
+# Python:  2.7.11
+#
+# Author:  Michael Harrison
+#
+# Version: 1.1
+#
+# Purpose: Understand the utilization of Python range() function
+#
+#
+
+## Use Python range() function with 1 parameter to display: 0,1,2,3
+for i in range(4):
+    print(i)
+
+## Use Python range() function with 3 parameters to display: 3,2,1,0
+for i in range(3, -1 , -1):
+    print(i)
+
+## Use Python range() function with 3 parameters to display: 8,6,4,2
+for i in range (8, 0, -2):
+    print (i)
